e287203ebd9995815c4350c63ad6c19a103d281e
gopikappukuttan <gopika.pa@edstem.com>
2025-05-20 11:50:52 +0530

    modifications

--- src/main/java/com/edstem/blog_api_exception_handling/config/SecurityConfig.java
+++ src/main/java/com/edstem/blog_api_exception_handling/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 				.authorizeHttpRequests(auth -> auth
-						.requestMatchers("/books/**").authenticated()
+						.requestMatchers("/posts/**").authenticated()
 						.anyRequest().permitAll()
 				)
 				.httpBasic(Customizer.withDefaults())
